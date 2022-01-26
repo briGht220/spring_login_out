ada22b1ba40ef0ed2083c6c93f1996cae911ad5c
briGht220 <jjw0104@gmail.com>
2022-01-26 23:35:01 +0900

    Spring LogIn, LogOut Project     Making Database table and program DB connection

---
+++ src/main/java/spring/config/AppContext.java
@@ -0,0 +1,22 @@
+package spring.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppContext {
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/logio");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        dataSource.setInitialSize(2);
+        dataSource.setMaxActive(10);
+
+        return dataSource;
+    }
+}
---
+++ src/main/java/spring/object/MemberDAO.java
@@ -0,0 +1,17 @@
+package spring.object;
+
+import java.util.Collection;
+
+public class MemberDAO {
+    public MemberDTO selectByEmail(String email) {
+        return null;
+    }
+
+    public void insert(MemberDTO memberDTO) {}
+
+    public void update(MemberDTO memberDTO) {}
+
+    public Collection<MemberDTO> selectAll() {
+        return null;
+    }
+}
