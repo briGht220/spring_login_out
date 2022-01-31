6b8024321bc0f19439e98bf2d534dd79abd1b958
briGht220 <jjw0104@gmail.com>
2022-01-31 19:59:58 +0900

    insert test...success

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,32 @@
 package main;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import spring.object.MemberDAO;
+import spring.object.MemberDTO;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        DataSource dataSource = new DataSource();
+        MemberDTO memberDTO = new MemberDTO();
 
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/logio");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        dataSource.setInitialSize(2);
+        dataSource.setMaxActive(100);
+
+        memberDTO.setEmail("test01@email.com");
+        memberDTO.setPassword("1111");
+        memberDTO.setName("Tester");
+
+        try {
+            MemberDAO memberDAO = new MemberDAO(dataSource, memberDTO);
+            System.out.println(memberDAO.insert_member());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
